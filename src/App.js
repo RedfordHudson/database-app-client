c25fa997f9f17baf87e8336a1d4bdd4b670eaeca
RedfordHudson <redfordfoxhudson@gmail.com>
2022-07-03 10:51:47 -0700

    primordial connection

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={callAPI}>Fetch</button>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+function callAPI() {
+  // const URL = 'http://localhost:3001';
+  // const URL = 'https://database-app-server/herokuapp.com';
+  const URL = 'https://database-app-server.herokuapp.com/'
+  fetch(URL, { method: 'GET' })
+    .then(response => response.json())
+    .then(json => alert(JSON.stringify(json)));
+}
+
+export default App;
